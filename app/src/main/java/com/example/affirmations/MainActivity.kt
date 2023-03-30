a9a3e6956a3d92c777e0e864f8b404fb66fbdfd0
Sourabh Kumar <20010001030@gateway.edu.in>
2023-03-30 23:59:52 +0530

    Created the list

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -23,6 +23,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -34,8 +36,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import javax.sql.DataSource
 
 
 class MainActivity : ComponentActivity() {
@@ -69,6 +73,15 @@ fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+private fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation)
+        }
+    }
+}
+
 @Preview
 @Composable
 private fun AffirmationCardPreview() {
@@ -78,5 +91,6 @@ private fun AffirmationCardPreview() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
     }
 }
